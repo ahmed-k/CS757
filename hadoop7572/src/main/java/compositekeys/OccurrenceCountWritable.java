55492c704c4337ec56a6b8f55ad5c791f3332c81
Ahmed Khalid <theak99@gmail.com>
2015-03-17 02:41:46 -0400

    fixed NPE in OccurrenceCountWritable put

--- hadoop7572/src/main/java/compositekeys/OccurrenceCountWritable.java
+++ hadoop7572/src/main/java/compositekeys/OccurrenceCountWritable.java
@@ -19,16 +19,17 @@ public class OccurrenceCountWritable extends MapWritable {
         for (Map.Entry<Writable, Writable> e : arr.entrySet()) {
             Writable key = e.getKey();
             Writable _myVal = this.get(e.getKey());
+            Writable _oVal = e.getValue();
             if (_myVal != null) {
                 IntWritable myVal = (IntWritable) _myVal;
-                IntWritable oVal = (IntWritable) e.getValue();
+                IntWritable oVal = (IntWritable) _oVal;
                 int my = myVal.get();
                 int o = oVal.get();
                 IntWritable update = new IntWritable(my+o);
                 this.put(key,update);
             }
             else {
-                this.put(key, _myVal);
+                this.put(key, _oVal);
             }
 
         }
