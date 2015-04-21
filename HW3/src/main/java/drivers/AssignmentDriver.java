aa441c1d5455e1f864bade4a2766a85e649fa807
ahmed-k <theak99@gmail.com>
2015-04-21 18:45:01 -0400

    hw3 matrix multiplier first cut added

---
+++ HW3/src/main/java/drivers/AssignmentDriver.java
@@ -0,0 +1,50 @@
+package drivers;
+
+import mappers.MatrixMultiplier;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by alabdullahwi on 4/21/2015.
+ */
+public class AssignmentDriver {
+
+    public static void main(String[] args) throws Exception {
+        //get args
+        String UDim = args[2];
+        String VDim = args[3];
+        String intDim = args[4];
+        //prepare job config object
+        Configuration conf = new Configuration();
+        conf.set("UDim", UDim);
+        conf.set("VDim", VDim);
+        conf.set("intDim", intDim);
+        //create job from config object
+        Job job = configureJob(conf);
+        //set input and output paths then exit
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true)? 0 :1);
+    }
+
+    public static Job configureJob(Configuration conf ) throws IOException {
+        Job job = new Job(conf, "batch gradient descent");
+        job.setJarByClass(AssignmentDriver.class);
+        job.setInputFormatClass(KeyValueTextInputFormat.class);
+        job.setMapperClass(MatrixMultiplier.MatrixMultiplierMapper.class);
+        job.setReducerClass(MatrixMultiplier.MatrixMultiplierReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+        return job;
+    }
+
+
+}
